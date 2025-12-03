e2fdc520293c14c9cc954e21017842f4725113cd
PrinceRaj9814 <princerajo9814@gmail.com>
2025-12-03 15:16:47 +0530

    Add files via upload

---
+++ BMI.py
@@ -0,0 +1,9 @@
+
+import streamlit as st
+
+weight = st.number_input("Enter in m")
+height = st.number_input("Enter : kg")
+
+if st.button('calculate'):
+    bmi = weight / (height ** 2)
+    st.write(bmi)
